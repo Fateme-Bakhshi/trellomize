40cb9753b809b813a6485869860291140582d8b5
Yasaman-Saffar <154500788+Yasaman-Saffar@users.noreply.github.com>
2024-05-20 09:03:37 -0700

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,9 +1,11 @@
 import argparse #terminal management
 import json 
+import os
 from pathlib import Path
 
+
 manager_file = Path('Data\\Manager.json')
-users_file = Path('Data\\Users.json')
+users_file = Path('Data\\Users')
 #project file
 #task file
 
@@ -20,7 +22,7 @@ def find_manager(Username):
     try:
         with open(manager_file, 'r') as input:
             manager = json.load(input)
-            if manager[0].get('username') == Username:
+            if manager[0].get('Username') == Username:
                 return True
             return False
     except Exception as error:
@@ -35,24 +37,24 @@ def save_manager(Username, Password):
             print(f'There is an error: {error}')
             
         
-        
-        
 def purge_data():
     confirmation = input('Are you sure you want to purge all data? (yes/no):')
-    try:
-        if confirmation.lower() == 'yes':
-            for data_path in [users_file #, project_file, #task_file 
-                        ]:
-                with open(data_path, 'w') as f:
-                    f.write('[]')
-                print('All data purged.')
-        elif confirmation.lower() == 'no':
-            print('Operation cancelled.')
-        else:
-            print('Please enter a valid answer.')
-            purge_data()
-    except Exception as error:
-        print(f'An uexpected error occured: {error}')
+    while True:
+        try:
+            if confirmation.lower() == 'yes':
+                for data_path in [users_file #, project_file, #task_file 
+                            ]:
+                    for filename in os.listdir(data_path): #Get a list of all the files in the desired folder
+                        file_path = os.path.join(data_path, filename)
+                        os.unlink(file_path) #deleting the data
+                    print('All data purged.')
+                    break
+            elif confirmation.lower() == 'no':
+                print('Operation cancelled.')
+            else:
+                print('Please enter a valid answer.')
+        except Exception as error:
+            print(f'An uexpected error occured: {error}')
         
         
 if __name__ == '__main__':
