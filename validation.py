ebb1779d8767ee8b11bb8d353b3cc63368d23b4d
Yasaman-Saffar <154500788+Yasaman-Saffar@users.noreply.github.com>
2024-05-22 14:41:39 -0700

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,44 +1,47 @@
 from Users.user import UserManager
+import os
 import re #for checking email
+import time
 
 class validService:
     def __init__(self):
-        self.user_manager = UserManager
-    
-    def is_valid_email(Email):
-        regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        self.user_manager = UserManager()
 
-        if re.match(regex, Email):
-            return True
-        else:
-            return False
+    def is_valid_email(self, email):
+        regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
+        return re.match(regex, email)
+
         
     def sign_Up(self, Username, Password, Email):
-        while True:
-            if(self.is_valid_email(Email)):
-                try:
-                    user = self.user_manager.add_user(Username, Password, Email)
-                    print(f'You signed up successfully {Username}!')
-                    return user
-                except ValueError as error:
-                    print(error)
-            else:
-                print('Please enter a valid email.')
+        while not self.is_valid_email(Email):
+            Email = input('Please enter a valid email: ')
+        try:
+            user = self.user_manager.add_user(Username, Password, Email)
+            print(f'You signed up successfully {Username}!')
+            time.sleep(1.5)
+            return user
+        except ValueError as error:
+            print(error)
+            return None
+    
     
     def log_in(self, Username, Password):
         user = self.user_manager.load_user(Username, Password)
-        while True:
-            try:
-                if user:
-                    if user.active :
-                        print(f'Welcome back {Username}!')
-                        return user
-                    else:
-                        print(f'{Username} is inactivate.')
+        try:
+            if user:
+                if user.activate :
+                    print(f'Welcome back {Username}!')
+                    time.sleep(2)
+                    return user
                 else:
-                    print('Invalid username or password.')
-            except Exception as error:
-                print(f'An unexpected error occured: {error}')
+                    print(f'{Username} is inactivate.')
+                    time.sleep(2)
+            else:
+                print('Invalid username or password.')
+                time.sleep(2)
+        except Exception as error:
+            print(f'An unexpected error occured: {error}')
+    
     
     def deactivate_user(self, Username, Password):
         user = self.user_manager.load_user(Username, Password)
