0df95353fb40e09bab7e1e249b74de14a7129e44
Yasaman-Saffar <154500788+Yasaman-Saffar@users.noreply.github.com>
2024-05-27 14:06:18 -0700

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -34,18 +34,35 @@ class main:
         for option, title in enumerate(choices):
             console.print(Panel(title,title=str(option+1),title_align="center", width=width, border_style="bold deep_sky_blue1"), justify="center")
             time.sleep(0.2)
-            
+    
+    def exit_program(self):
+        self.clear_screen()
+        self.print_slowly("\nThanks for using this program!", 0.04)
+        time.sleep(0.5)
+        exit(1)
+    
+    def back_or_continue(self):
+        console = Console()
+        choices = ['Continue', 'Back']
+        self.display_choices(self, choices, 12)
+        choice = input()
+        while True:
+            if choice == '0' or choice == '1':
+                return int(choice)
+            else:
+                choice = input('Please enter a valid number(0 or 1): ')
+                    
     def log_or_sign(self):
         self.clear_screen()
         
         self.print_slowly("\nWelcome to Project Management System!", 0.04)
         
         while True:
-            self.show_title("[bold deep_sky_blue3]Login/Sign Up")
+            self.show_title("[bold deep_sky_blue1]Login/Sign Up")
             print("\n")
             self.display_choices(['Login', 'Sign Up', 'Exit'], 12)
-            choice = input("                                                          Enter your choice: ")
-            
+            choice = input("                                                                      Enter your choice: ")
+              
             while True:
                 if(choice == '1'):
                     self.login()
@@ -54,32 +71,36 @@ class main:
                     self.sign_up()
                     break
                 elif(choice == '3'):
-                    self.clear_screen()
-                    self.print_slowly("\nThanks for using this program!", 0.04)
-                    time.sleep(0.5)
-                    exit(1)
+                    self.exit_program()
                 else:
-                    choice = input("                                                   Please enter a valid number(1-3): ")
+                    choice = input("                                                             Please enter a valid number(1-3): ")
         
     def login(self):
-        con = Console()
         self.show_title("[bold deep_sky_blue3]Login")
         
         username = Prompt.ask("Enter your Username")
         password = Prompt.ask("Enter your Password", password=True)
+        print('\n')
         self.user = self.valid_service.log_in(username, password)
         if self.user:
             self.user_dashboard()
+        else:
+            if self.back_or_continue():
+                self.login()
         
     def sign_up(self):
-        con = Console()
         self.show_title("[bold deep_sky_blue3]Sign Up")
         
         username = Prompt.ask("Enter the Username")
         password = Prompt.ask("Enter the Password", password=True)
-        email = Prompt.ask("Enter the Password", default="example@gmail.com")
+        email = Prompt.ask("Enter the Email", default="example@gmail.com")
+        print('\n')
         
-        self.valid_service.sign_Up(username, password, email)
+        user = self.valid_service.sign_Up(username, password, email)
+        if not user:
+            if self.back_or_continue():
+                self.sign_up()
+            
     
     def user_dashboard(self):
         while True:
@@ -95,12 +116,12 @@ class main:
                 self.display_choices(choices, 20)
             
 
-            choice2 = input('                                                         Enter your choice: ')
+            choice2 = input('                                                                     Enter your choice: ')
             while True:
                 if (choice2 == '3' and not self.user.is_Manager):
                     return
                 if (choice2 == '4' and not self.user.is_Manager):
-                    exit(1)
+                    self.exit_program()
                 if (choice2 == '3' and self.user.is_Manager):
                     self.deactivating_user()
                     break
@@ -110,15 +131,16 @@ class main:
                 if (choice2 == '5' and self.user.is_Manager):
                     return
                 if (choice2 == '6' and self.user.is_Manager):
-                    exit(1)
+                    self.exit_program()
                 else:
-                    choice2 = input('                                                          Please enter a valid number: ')
+                    choice2 = input('                                                                  Please enter a valid number: ')
                 
     def deactivating_user(self):
         self.show_title("[bold deep_sky_blue3]Deactivating User")
         
         username = Prompt.ask('Enter the userename you want to deactivate')
         password = Prompt.ask('Enter the password of the account', password=True)
+        print('\n')
         self.valid_service.deactivate_user(username, password)
 
     def activating_user(self):
@@ -126,6 +148,7 @@ class main:
         
         username = Prompt.ask('Enter the userename you want to activate')
         password = Prompt.ask('Enter the password of the account', password=True)
+        print('\n')
         self.valid_service.activate_user(username, password)
         
         
